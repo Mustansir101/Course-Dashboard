69245997f97e87df4f6ec61d04690e96f9bca8da
Mustansir101 <agasi.mustansir@gmail.com>
2025-10-15 23:49:12 +0530

    host

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://dashboard-frontend-pxpz.onrender.com"],
     credentials: true,
   })
 );
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,7 +9,8 @@ import {
 } from "../types";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL ||
+  "https://course-dashboard-dkff.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
